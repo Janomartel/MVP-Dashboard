3b9529f0acb35d009e31b3a5c34047cc9fe66111
Janomartel <alejan.martel@gmail.com>
2025-11-26 21:54:24 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -308,6 +308,8 @@ if not df_battery.empty:
     # Mapear device_ids a nombres
     device_id_to_name = {did: name for did, name in zip(device_ids, device_names)}
     df_battery["nombre_dispositivo"] = df_battery["device_id"].map(device_id_to_name)
+    df_battery = df_battery.sort_values("battery", ascending=False)
+
 
     st.dataframe(
         df_battery[["nombre_dispositivo", "battery", "timestamp"]].rename(columns={
